9625e6e855adf1af826c080de51f2232a1ce2bc5
johnwlockwood <johnwlockwood@gmail.com>
2013-10-14 23:31:40 -0700

    Move code to main functions
    This code looks like it's designed to run only when executed such as with: python main.py
    but this can run the code if it's imported as a module. When a python file
    is executed, the global name __name__ will be defined as '__main__'.

--- linetest.py
+++ linetest.py
@@ -3,20 +3,25 @@ from pypdflite.pdfobjects.pdfcolorscheme import PDFColorScheme
 from pypdflite.pdfobjects.pdfcolor import PDFColor
 
 
-Writer = PDFLite("LinesTest.pdf")
-#Writer.setCompression(True)
-Writer.setInformation(title="Testing Lines")  # set optional information
-Document = Writer.getDocument()
-Document.addLine(20, 40, 300, 80)
+def main():
+    Writer = PDFLite("LinesTest.pdf")
+    #Writer.setCompression(True)
+    Writer.setInformation(title="Testing Lines")  # set optional information
+    Document = Writer.getDocument()
+    Document.addLine(20, 40, 300, 80)
 
-Document.addText("Testing")
-Document.addNewline(1)
-Document.drawHorizonalLine()
+    Document.addText("Testing")
+    Document.addNewline(1)
+    Document.drawHorizonalLine()
 
-lightblue = PDFColor(name='lightblue')
-newscheme = PDFColorScheme(fillcolor=lightblue)
+    lightblue = PDFColor(name='lightblue')
+    newscheme = PDFColorScheme(fillcolor=lightblue)
 
-Document.setColorScheme(newscheme)
-Document.drawRectangle(150, 500, 300, 600, style='B')
+    Document.setColorScheme(newscheme)
+    Document.drawRectangle(150, 500, 300, 600, style='B')
 
-Writer.close()
+    Writer.close()
+
+
+if __name__ == '__main__':
+    main()
