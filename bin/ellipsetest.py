f995fde214e73856d2b93b789df3f274b91ccaca
tjoneslo <tjoneslow@gmail.com>
2014-03-16 12:27:31 -0400

    create new pdfellipse object for drawing ellipses (and circles). Refactor PDFLine, PDFRectangle, and PDFEllipse into a PDFDraw base class.

---
+++ bin/ellipsetest.py
@@ -0,0 +1,56 @@
+'''
+Created on Mar 15, 2014
+
+@author: tjoneslo
+'''
+from pypdflite.pdflite import PDFLite
+from pypdflite.pdfobjects.pdfcolor import PDFColor
+from pypdflite.pdfobjects.pdfcursor import PDFCursor
+from pypdflite.pdfobjects.pdfellipse import PDFEllipse
+
+def EllipseTest():
+    '''
+    Functional test for drawing eclipses
+    '''
+    # Create PDFLite object
+    writer = PDFLite("generated/EclipseTest.pdf")
+
+    # Set compression defaults to False
+    writer.set_compression(False)
+
+    # Set document metadata
+    writer.set_information(title="Testing Lines")  # set optional information
+
+    # Get documnet object
+    document = writer.get_document()
+
+    color  = PDFColor(name = 'red')
+    center = PDFCursor(100, 100)
+    radius = PDFCursor(10,10)
+    
+    circle = PDFEllipse (document.session, document.page, center, radius, color)
+    circle._draw()
+    
+    center.x = 200
+    center.y = 200
+    circle = PDFEllipse (document.session, document.page, center, radius, color, stroke='F')
+    circle._draw()
+    
+    center.x=100
+    center.y=200
+    radius.x=20
+    circle = PDFEllipse(document.session, document.page, center, radius, color, size=3)
+    circle._draw()
+    radius.x = 10
+    radius.y = 20
+    
+    color.set_color_by_name('green')
+    circle._draw()
+    
+
+    # Close Document
+    writer.close()
+
+
+if __name__ == '__main__':
+    EllipseTest()
