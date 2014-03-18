b0eaf9a942735122c5c63be4f67eee2b2395c1ac
tjoneslo <tjoneslow@gmail.com>
2014-03-17 20:35:10 -0400

    Revert "Fixed runexamples"
    This reverts commit 0da480d804417da85c47f83c80089f7058f6bba7.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='PyPDFLite',
-    version='0.1.20',
+    version='0.1.19',
     author='Katerina Hanson',
     author_email='katerina.hanson@gmail.com',
     packages=['pypdflite', 'pypdflite.pdfobjects'],
-    scripts=['runexamples.py'],
+    scripts=['bin/runexamples.py'],
     license='LICENSE.txt',
     description='Simple PDF Writer.',
     long_description=open('README.txt').read(),
