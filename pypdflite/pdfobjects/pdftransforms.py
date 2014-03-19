bc59d779fe0704fba01a4b4391f8c0962086af7a
tjoneslo <tjoneslow@gmail.com>
2014-03-18 22:14:46 -0400

    Add methods to do text transformations (rotate, skew, move, scale) to the PDFText method to allow many more text options.
    Add a new PDFTransforms to allow similar transformations on graphics
    (not text), though the graphics support may not currently need it.

--- pypdflite/pdfobjects/pdftransforms.py
+++ pypdflite/pdfobjects/pdftransforms.py
@@ -20,6 +20,7 @@ class PDFTransform(object):
         self.page = page
 
         self._currentMatrix = (1., 0., 0., 1., 0., 0.)
+        self._textMatrix = (1., 0., 0., 1., 0., 0.)
 
     def saveState(self):
         """Save the current graphics state to be restored later by restoreState.
@@ -37,11 +38,11 @@ class PDFTransform(object):
         
         
         """
-        self.session._out('q')
+        self.session._out('q', self.page)
 
     def restoreState(self):
         """restore the graphics state to the matching saved state (see saveState)."""
-        self.session._out('Q')
+        self.session._out('Q', self.page)
 
         
     def transform(self, a,b,c,d,e,f):
@@ -52,8 +53,8 @@ class PDFTransform(object):
         self._currentMatrix = (a0*a+c0*b,    b0*a+d0*b,
                                a0*c+c0*d,    b0*c+d0*d,
                                a0*e+c0*f+e0, b0*e+d0*f+f0)
-
-        self.session._out ('%.2f %.2f %.2f %.2f %.2f %.2f cm' % (a,b,c,d,e,f))
+        a1,b1,c1,d1,e1,f1 = self._currentMatrix
+        self.session._out ('%.2f %.2f %.2f %.2f %.2f %.2f cm' % (a1,b1,c1,d1,e1,f1), self.page)
 
     def absolutePosition(self, x, y):
         """return the absolute position of x,y in user space w.r.t. default user space"""
@@ -83,4 +84,3 @@ class PDFTransform(object):
         tanAlpha = tan(alpha * pi / 180)
         tanBeta  = tan(beta  * pi / 180)
         self.transform(1, tanAlpha, tanBeta, 1, 0, 0)
-    
