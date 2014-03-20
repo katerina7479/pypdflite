d54b94f4ab0a00dc5820b105ad0a5282f0f3b758
Katerina Hanson <katerina.hanson@gmail.com>
2014-03-20 12:30:27 -0700

    PEP 8

--- pypdflite/pdfobjects/pdftransforms.py
+++ pypdflite/pdfobjects/pdftransforms.py
@@ -12,7 +12,7 @@ class PDFTransform(object):
     '''
 
 
-    def __init__(self,  session, page):
+    def __init__(self, session, page):
         '''
         Constructor
         '''
@@ -45,34 +45,34 @@ class PDFTransform(object):
         self.session._out('Q', self.page)
 
         
-    def transform(self, a,b,c,d,e,f):
+    def transform(self, a, b, c, d, e, f):
         """ Adjust the current transformation state of the current graphics state
         matrix. Not recommended for the faint of heart. 
         """
-        a0,b0,c0,d0,e0,f0 = self._currentMatrix
-        self._currentMatrix = (a0*a+c0*b,    b0*a+d0*b,
-                               a0*c+c0*d,    b0*c+d0*d,
-                               a0*e+c0*f+e0, b0*e+d0*f+f0)
-        a1,b1,c1,d1,e1,f1 = self._currentMatrix
-        self.session._out ('%.2f %.2f %.2f %.2f %.2f %.2f cm' % (a1,b1,c1,d1,e1,f1), self.page)
+        a0, b0, c0, d0, e0, f0 = self._currentMatrix
+        self._currentMatrix = (a0 * a + c0 * b, b0 * a + d0 * b,
+                               a0 * c + c0 * d, b0 * c + d0 * d,
+                               a0 * e+ c0 * f + e0, b0 * e + d0 * f + f0)
+        a1, b1, c1, d1, e1, f1 = self._currentMatrix
+        self.session._out('%.2f %.2f %.2f %.2f %.2f %.2f cm' % (a1, b1, c1, d1, e1, f1), self.page)
 
     def absolutePosition(self, x, y):
         """return the absolute position of x,y in user space w.r.t. default user space"""
-        (a,b,c,d,e,f) = self._currentMatrix
-        xp = a*x + c*y + e
-        yp = b*x + d*y + f
+        (a, b, c, d, e, f) = self._currentMatrix
+        xp = a * x + c * y + e
+        yp = b * x + d * y + f
         return (xp, yp)
 
     def translate(self, dx, dy):
         """move the origin from the current (0,0) point to the (dx,dy) point
            (with respect to the current graphics state)."""
-        self.transform(1,0,0,1,dx,dy)
+        self.transform(1, 0, 0, 1, dx, dy)
 
     def scale(self, x, y):
         """Scale the horizontal dimension by x and the vertical by y
            (with respect to the current graphics state).
            For example canvas.scale(2.0, 0.5) will make everything short and fat."""
-        self.transform(x,0,0,y,0,0)
+        self.transform(x, 0, 0, y, 0, 0)
 
     def rotate(self, theta):
         """Rotate current graphic state by the angle theta (in degrees)."""
