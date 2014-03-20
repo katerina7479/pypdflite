fa3760876aeb8752b6429094e7f7fb72f0ec2b32
Katerina Hanson <katerina.hanson@gmail.com>
2014-03-20 14:25:22 -0700

    Added html test

---
+++ bin/htmltest.py
@@ -0,0 +1,29 @@
+from pypdflite.pdflite import PDFLite
+
+
+def HtmlTest():
+    writer = PDFLite(reportfile, font_dir=settings.REPORTING_FONT_DIR)
+    document = writer.get_document()
+
+    document.add_text('Sample text')
+    document.add_newline(2)
+
+
+    red = PDFColor("red")
+
+    normalfont = document.get_font()
+    header1 = document.set_font('helvetica', style='B', size=24)
+    coolfont = document.set_font('comic sans ms', style='', size=12)
+
+    document.add_html("""<h1>This is h1 text.</h1>
+                    <p>This is a sample paragraph with a formatted variable equal to <span class="red coolfont" data-bind="myvar"></span></p>
+                    """, context={"myvar": 5}, formatting={"h1": header1, "red": red, "coolfont": coolfont, "p": normalfont}
+    )
+
+    document.add_newline(2)
+    document.add_text("After HTML.")
+    writer.close()
+
+
+if __name__ == "__main__":
+    HtmlTest()
