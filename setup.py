4d686db84491edc8617a88834d52b24de9b2ceb5
Katerina Hanson <katerina.hanson@gmail.com>
2014-03-25 11:59:12 -0700

    For package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyPDFLite',
-    version='0.1.25',
+    version='0.1.26',
     author='Katerina Hanson',
     author_email='katerina.hanson@gmail.com',
     packages=['pypdflite', 'pypdflite.pdfobjects'],
