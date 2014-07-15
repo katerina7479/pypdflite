1b12f64138a43f544eda18cd8f1ec0d0fdd9ab78
Katerina Hanson <katerina.hanson@gmail.com>
2014-07-15 16:10:02 -0700

    Added legend to xyscatter test

--- bin/xyscatterplot.py
+++ bin/xyscatterplot.py
@@ -21,7 +21,7 @@ def XYScatterPlotTest():
     cursor = PDFCursor(100, 50)
     data = [{"series1": [(14.2, 215), (16.4, 325), (11.9, 185), (15.2, 332), (18.5, 406), (22.1, 522), (19.4, 412), (25.1, 614), (23.4, 544), (18.1, 421), (22.6, 445), (17.2, 408)]}]
 
-    document.add_xy_scatter(data, cursor, 400, 200, "Ice Cream Sales vs Temperature", None, None, None, ("temperature", "sales"), "Auto", padding=0.11, dots=1)
+    document.add_xy_scatter(data, cursor, 400, 200, "Ice Cream Sales vs Temperature", (10, 26), (180, 660), (1, 50), ("temperature", "sales"), "Auto", padding=0.11, dots=1, legend="right")
 
     cursor = PDFCursor(100, 400)
     document.add_xy_scatter(data, cursor, 400, 200, "Ice Cream Sales vs Temperature", None, None, None, ("temperature", "sales"), "Auto", padding=0.11, dots=1, linear_regression=True, linear_regression_equation=True)
