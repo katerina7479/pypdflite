9aaf8352769075c94361e9df5d009e8c7fb84256
Katerina Hanson <katerina.hanson@gmail.com>
2014-07-16 12:44:07 -0700

    Fixed line calculation

--- pypdflite/pdfobjects/pdfxyscatter.py
+++ pypdflite/pdfobjects/pdfxyscatter.py
@@ -21,17 +21,32 @@ class PDFXYScatter(PDFLineGraph):
 
         i = 0
         for series in self.data:
-            self._set_color(i)
             if self.legend is not None:
                 self._draw_legend_line(i, series.keys()[0])
-                i += 1
+            series = series.values()[0]
+            self._set_color(i)
+            self.line = LinearRegressionLine()
+            i += 1
 
-            for values in series.itervalues():
-                cursors = []
-                for pair in values:
-                    cursor = self.get_coord(pair)
-                    cursors.append(cursor)
-                self._draw_dots(cursors)
+            cursors = []
+            for value in series:
+                cursor = self.get_coord(value)
+                cursors.append(cursor)
+                self.line.add_data(value)
+            self._draw_dots(cursors)
+
+            if self.linear_regression:
+                self.line.calculate_line()
+                cursor1, cursor2 = self.line.get_cursors(cursors)
+                trend = PDFLine(self.session, self.page, cursor1, cursor2)
+                trend._draw()
+                if self.linear_regression_equation:
+                    text = self.line.get_equation()
+                    text_width = self.session.parent.document.font._string_width(text)
+                    text_height = self.session.parent.document.font.font_size * 1.2
+                    x = cursor2.x + (-text_width)
+                    y = self.line._get_y_at_x(x) + text_height
+                    PDFText(self.session, self.page, text, cursor=PDFCursor(x, y))
 
         if self.legend is not None:
             self._draw_legend_box()
@@ -47,26 +62,10 @@ class PDFXYScatter(PDFLineGraph):
         self.legend_data_start.y_plus(1.75 * line_height)
 
     def _draw_dots(self, cursors):
-        if self.linear_regression:
-            self.linear_regression_line = LinearRegressionLine()
         if self.dots is not None:
             for cursor in cursors:
                 dot = PDFEllipse(self.session, self.page, cursor, PDFCursor(self.dots, self.dots), stroke="F")
                 dot._draw()
-                if self.linear_regression:
-                    self.linear_regression_line.add_coord(cursor)
-            if self.linear_regression:
-                self.linear_regression_line.calculate_line()
-                cursor1, cursor2 = self.linear_regression_line.get_cursors()
-                line = PDFLine(self.session, self.page, cursor1, cursor2)
-                line._draw()
-                if self.linear_regression_equation:
-                    text = self.linear_regression_line.get_equation()
-                    text_width = self.session.parent.document.font._string_width(text)
-                    text_height = self.session.parent.document.font.font_size * 1.2
-                    x = cursor2.x + (-text_width)
-                    y = self.linear_regression_line._get_y_at_x(x) + text_height
-                    text = PDFText(self.session, self.page, text, cursor=PDFCursor(x, y))
 
     def _set_color(self, index):
         color = self.line_colors[index]
@@ -80,13 +79,8 @@ class PDFXYScatter(PDFLineGraph):
                 self.session._out(color._get_color_string(), self.page)
                 self.session._save_color(color.copy())
 
-
 class LinearRegressionLine(object):
     def __init__(self):
-        self.min_x = None
-        self.min_y = None
-        self.max_x = None
-        self.max_y = None
         self.x_sum = 0
         self.y_sum = 0
         self.xy_sum = 0
@@ -94,9 +88,9 @@ class LinearRegressionLine(object):
         self.y2_sum = 0
         self.N = 0
 
-    def add_coord(self, cursor):
-        x = cursor.x
-        y = cursor.y
+    def add_data(self, data):
+        x = data[0]
+        y = data[1]
         self.x_sum += x
         self.y_sum += y
         self.xy_sum += x * y
@@ -104,56 +98,76 @@ class LinearRegressionLine(object):
         self.y2_sum += y ** 2
         self.N += 1
 
-        if self.min_x is None:
-            self.min_x = x
-        elif x < self.min_x:
-            self.min_x = x
-
-        if self.max_x is None:
-            self.max_x = x
-        elif x > self.max_x:
-            self.max_x = x
-
-        if self.min_y is None:
-            self.min_y = y
-        elif y < self.min_y:
-            self.min_y = y
-
-        if self.max_y is None:
-            self.max_y = y
-        elif y > self.max_y:
-            self.max_y = y
-
     def calculate_line(self):
-        self.slope = ((self.N * self.xy_sum) - (self.x_sum * self.y_sum)) / ((self.N * self.x2_sum) - (self.x_sum ** 2))
-        self.intercept = ((self.x2_sum * self.y_sum) - (self.x_sum * self.xy_sum)) / ((self.N * self.x2_sum) - (self.x_sum ** 2))
+        self.data_slope = ((self.N * self.xy_sum) - (self.x_sum * self.y_sum)) / ((self.N * self.x2_sum) - (self.x_sum ** 2))
+        self.data_intercept = ((self.x2_sum * self.y_sum) - (self.x_sum * self.xy_sum)) / ((self.N * self.x2_sum) - (self.x_sum ** 2))
+        return self.data_slope, self.data_intercept
 
-        return self.slope, self.intercept
+    def get_cursors(self, cursors):
+        xlist = [i.x for i in cursors]
+        ylist = [i.y for i in cursors]
 
-    def get_cursors(self):
+        min_x = min(xlist)
+        min_y = min(ylist)
+        max_x = max(xlist)
+        max_y = max(ylist)
+
+        x_sum = 0
+        y_sum = 0
+        xy_sum = 0
+        x2_sum = 0
+        y2_sum = 0
+        N = 0
+
+        for cursor in cursors:
+            x = cursor.x
+            y = cursor.y
+            x_sum += x
+            y_sum += y
+            xy_sum += x * y
+            x2_sum += x ** 2
+            y2_sum += y ** 2
+            N += 1
+
+        self.cursor_slope = ((N * xy_sum) - (x_sum * y_sum)) / ((N * x2_sum) - (x_sum ** 2))
+        self.cursor_intercept = ((x2_sum * y_sum) - (x_sum * xy_sum)) / ((N * x2_sum) - (x_sum ** 2))
+
         x1 = 0
         y1 = 0
-        if self.intercept < self.min_y:
-            y1 = self.min_y
+        if self.cursor_intercept < min_y:
+            y1 = min_y
             x1 = self._get_x_at_y(y1)
-        if self.intercept >= self.min_y:
-            x1 = self.min_x
+        if self.cursor_intercept >= min_y:
+            x1 = min_x
             y1 = self._get_y_at_x(x1)
 
-        x2 = self.max_x
+        x2 = max_x
         y2 = self._get_y_at_x(x2)
-        if y2 > self.max_y:
-            y2 = self.max_y
+        if y2 > max_y:
+            y2 = max_y
             x2 = self._get_x_at_y(y2)
 
         return PDFCursor(x1, y1), PDFCursor(x2, y2)
 
     def _get_x_at_y(self, y):
-        return (y - self.intercept) / float(self.slope)
+        return (y - self.cursor_intercept) / float(self.cursor_slope)
 
     def _get_y_at_x(self, x):
-        return self.slope * x + self.intercept
+        return self.cursor_slope * x + self.cursor_intercept
 
     def get_equation(self):
-        equation =  "y = %0.2f x + %0.2f" % (self.slope, self.intercept)
+        equation =  "y = %0.2f x + %0.2f" % (self.data_slope, self.data_intercept)
         return equation
+
+    def interpolate(self, item, tuplelist):
+        index_high = 0
+        keys = [i[0] for i in tuplelist]
+        values = [i[1] for i in tuplelist]
+        for key in keys:
+            if item < key:
+                index_high = keys.index(key)
+                break
+        index_low = index_high - 1
+        ratio = (item - keys[index_low]) / float((keys[index_high] - keys[index_low]))
+        value = ratio * (values[index_high] - values[index_low]) + values[index_low]
+        return value
